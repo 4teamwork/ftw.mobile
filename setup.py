e5e74d1b7125890cddfa6c8a3248fd0dd02fa5ec
Martin Bächtold <martin@baechtold.me>
2017-10-24 17:31:48 +0200

    Start adding support for Plone 5.1.

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(name='ftw.mobile',
       classifiers=[
           'Framework :: Plone',
           'Framework :: Plone :: 4.3',
+          'Framework :: Plone :: 5.1',
           'License :: OSI Approved :: GNU General Public License (GPL)',
           'Programming Language :: Python',
           'Programming Language :: Python :: 2.7',
